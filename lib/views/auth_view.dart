df78931608748cd92d93bfd7474e5c102e18411f
jhoabran <69612177+jhoabran@users.noreply.github.com>
2025-02-03 22:27:30 -0400

    Add localization support to the app

--- lib/views/auth_view.dart
+++ lib/views/auth_view.dart
@@ -1,6 +1,9 @@
 import 'package:eventify/services/auth_service.dart';
 import 'package:eventify/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:eventify/models/locale.dart';
+import 'package:provider/provider.dart';
 
 class AuthView extends StatefulWidget {
   const AuthView({super.key});
@@ -10,37 +13,45 @@ class AuthView extends StatefulWidget {
 }
 
 class _AuthViewState extends State<AuthView> {
-  // get auth service
   final authService = AuthService();
-
-  // text controllers
   final _emailController = TextEditingController();
   final _passwordControler = TextEditingController();
 
-  // login button pressed
-  void login() async {
-    // prepare data
-    final email = _emailController.text;
-    final password = _passwordControler.text;
-
-    try {
-      await authService.signInWithEmailPassword(email, password, context);
-
-    } catch (e) {
-      if (mounted) {
-        ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: Text("Error: $e")));
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    final t = AppLocalizations.of(context)!;
+    final selectedLocale = Localizations.localeOf(context).toString();
+    
+    // login button pressed
+    void login() async {
+      // prepare data
+      final email = _emailController.text;
+      final password = _passwordControler.text;
+
+      try {
+        await authService.signInWithEmailPassword(email, password, context);
+
+      } catch (e) {
+        if (mounted) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text("Error: $e")));
+        }
+      }
+    }
+
     return Scaffold(
       body: Padding(padding: const EdgeInsets.all(16.0), 
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Consumer<LocaleModel>(
+              builder: (context, localeModel, child) =>
+              ElevatedButton(
+                onPressed: () => localeModel.set(Locale('es')),
+                child: Text('Spanish'),
+              ),
+          ),
+          Text('${t.helloWorld} $selectedLocale'),
           // email
           TextField(
             controller: _emailController,
