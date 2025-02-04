d8dc511cf79b4640b0f75f712af2eb38c1aa07ec
jhoabran <69612177+jhoabran@users.noreply.github.com>
2025-02-04 14:40:25 -0400

    fixing main dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,13 @@ import 'package:eventify/views/notifications_view.dart';
 import 'package:eventify/views/profile_view.dart';
 import 'package:eventify/navigation.dart';
 import 'package:eventify/routes.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:eventify/models/locale.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
   // supabase setup
