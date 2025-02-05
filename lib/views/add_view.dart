9e9c5d49c1391dc22c310e1aef776a7b7194d11d
ruggieroDaniela <chachy.drs@gmail.com>
2025-02-05 15:44:22 -0400

    setUp inicial

--- lib/views/add_view.dart
+++ lib/views/add_view.dart
@@ -1,3 +1,4 @@
+import 'package:eventify/widgets/app_header.dart';
 import 'package:flutter/material.dart';
 
 class AddView extends StatefulWidget {
@@ -8,67 +9,17 @@ class AddView extends StatefulWidget {
 }
 
 class _AddViewState extends State<AddView> {
-  final _formKey = GlobalKey<FormState>();
-  final _titleController = TextEditingController();
-  final _descriptionController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Agregar Nuevo'),
-      ),
-      body: Padding(
+      appBar: AppHeader(title: "Nuevo Evento"),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _titleController,
-                decoration: const InputDecoration(
-                  labelText: 'Título',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese un título';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: _descriptionController,
-                decoration: const InputDecoration(
-                  labelText: 'Descripción',
-                  border: OutlineInputBorder(),
-                ),
-                maxLines: 3,
-              ),
-              const SizedBox(height: 24),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Aquí puedes agregar la lógica para guardar los datos
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Procesando datos...')),
-                    );
-                  }
-                },
-                child: const Text('Guardar'),
-              ),
-            ],
-          ),
-        ),
+        
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _titleController.dispose();
-    _descriptionController.dispose();
-    super.dispose();
   }
 }
