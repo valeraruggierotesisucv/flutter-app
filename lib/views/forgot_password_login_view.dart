da7c492f138503b1d21394c8a47e6b602e6c7b0c
jhoabran <69612177+jhoabran@users.noreply.github.com>
2025-02-05 15:53:41 -0400

    FLUT-67 Add Forgot Password flow with localization support

--- lib/views/forgot_password_login_view.dart
+++ lib/views/forgot_password_login_view.dart
@@ -1,13 +1,69 @@
+import 'package:eventify/views/auth_view.dart';
+import 'package:eventify/widgets/app_header.dart';
+import 'package:eventify/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 class ForgotPasswordLoginView extends StatelessWidget {
   const ForgotPasswordLoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Forgot Password Login  View'),
+    final t = AppLocalizations.of(context);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppHeader(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                    t!.forgotPasswordLoginViewTitle,
+                    style: const TextStyle(
+                      fontSize: 28,
+                      fontFamily: 'SFProRounded',
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 40),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Image.asset(
+                        'assets/images/ForgotPassword.png',
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  Text(
+                    t.forgotPasswordLoginViewDescription,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'SFProText',
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 60),
+              child: CustomButton(
+                label: t.forgotPasswordLoginViewLogin,
+                onPress: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const AuthView())),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
