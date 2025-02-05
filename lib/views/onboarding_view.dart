0a3d0cab87772c1ca095f8092934faacda8c6e25
ruggieroDaniela <chachy.drs@gmail.com>
2025-02-04 22:27:50 -0400

    onboarding

--- lib/views/onboarding_view.dart
+++ lib/views/onboarding_view.dart
@@ -1,158 +1,57 @@
+import 'package:eventify/routes.dart';
+import 'package:eventify/widgets/custom_button.dart';
+import 'package:eventify/widgets/icon_logo.dart';
 import 'package:flutter/material.dart';
 
-class OnboardingView extends StatefulWidget {
+class OnboardingView extends StatelessWidget {
   const OnboardingView({super.key});
 
   @override
-  State<OnboardingView> createState() => _OnboardingViewState();
-}
-
-class _OnboardingViewState extends State<OnboardingView> {
-  final PageController _pageController = PageController();
-  int _currentPage = 0;
-
-  final List<OnboardingPage> _pages = [
-    OnboardingPage(
-      title: 'Bienvenido',
-      description: 'Descubre todas las funcionalidades de nuestra aplicación',
-      image: 'assets/onboarding1.png',
-    ),
-    OnboardingPage(
-      title: 'Características',
-      description: 'Explora nuestras increíbles características',
-      image: 'assets/onboarding2.png',
-    ),
-    OnboardingPage(
-      title: '¡Comencemos!',
-      description: 'Estás listo para empezar',
-      image: 'assets/onboarding3.png',
-    ),
-  ];
-
-  @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: PageView.builder(
-                controller: _pageController,
-                onPageChanged: (int page) {
-                  setState(() {
-                    _currentPage = page;
-                  });
-                },
-                itemCount: _pages.length,
-                itemBuilder: (context, index) {
-                  return _buildPage(_pages[index]);
-                },
-              ),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconLogo(
+                  height: 210,
+                  width: 230,
+                ),
+                Image.asset(
+                  'assets/images/Onboarding.png',
+                  height: height * 0.43, 
+                  fit: BoxFit.contain,
+                ),
+                Text(
+                  "Events for Everyone",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontFamily: 'SF-Pro-Rounded-Heavy',
+                    fontSize: 45,
+                    height: 1,                    
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                SizedBox(height: 20,), 
+                CustomButton(
+                  label: "Comenzar",
+                  onPress: () {
+                    Navigator.pushNamed(context, '/${AppScreens.auth.name}');
+                  },
+                  size: ButtonSize.large,
+                ),
+              ],
             ),
-            _buildPageIndicator(),
-            _buildButtons(),
-          ],
+            
+          ),
+          
         ),
       ),
     );
   }
-
-  Widget _buildPage(OnboardingPage page) {
-    return Padding(
-      padding: const EdgeInsets.all(40.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            page.image,
-            height: 300,
-          ),
-          const SizedBox(height: 40),
-          Text(
-            page.title,
-            style: const TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            page.description,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildPageIndicator() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(
-        _pages.length,
-        (index) => Container(
-          margin: const EdgeInsets.symmetric(horizontal: 4),
-          height: 8,
-          width: _currentPage == index ? 24 : 8,
-          decoration: BoxDecoration(
-            color: _currentPage == index ? Colors.blue : Colors.grey,
-            borderRadius: BorderRadius.circular(4),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildButtons() {
-    return Padding(
-      padding: const EdgeInsets.all(40.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          if (_currentPage > 0)
-            TextButton(
-              onPressed: () {
-                _pageController.previousPage(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeInOut,
-                );
-              },
-              child: const Text('Anterior'),
-            )
-          else
-            const SizedBox(width: 80),
-          TextButton(
-            onPressed: () {
-              if (_currentPage < _pages.length - 1) {
-                _pageController.nextPage(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeInOut,
-                );
-              } else {
-                // Navegar a la siguiente pantalla
-                Navigator.pushReplacementNamed(context, '/home');
-              }
-            },
-            child: Text(_currentPage < _pages.length - 1 ? 'Siguiente' : 'Comenzar'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class OnboardingPage {
-  final String title;
-  final String description;
-  final String image;
-
-  OnboardingPage({
-    required this.title,
-    required this.description,
-    required this.image,
-  });
 }
