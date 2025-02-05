e0085ef70da650e427108ff4ac4579ebef688d7f
ruggieroDaniela <chachy.drs@gmail.com>
2025-02-04 20:31:08 -0400

    notificationsView

--- lib/views/notifications_view.dart
+++ lib/views/notifications_view.dart
@@ -1,37 +1,81 @@
+import 'package:eventify/widgets/app_header.dart';
+import 'package:eventify/widgets/loading.dart';
+import 'package:eventify/widgets/notification_item.dart';
 import 'package:flutter/material.dart';
 
-class NotificationsView extends StatelessWidget {
-  const NotificationsView({super.key});
+class NotificationsView extends StatefulWidget {
+  const NotificationsView({super.key}); 
+
+  @override
+  State createState() => _NotificationsViewState();
+}
+
+class _NotificationsViewState extends State<NotificationsView> {
+  List notifications = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNotifications();
+  }
+
+  Future<void> fetchNotifications() async {
+    // Simulación de la obtención de notificaciones
+    notifications = [
+      {
+        "user": "Usuario1",
+        "userAvatar": "https://crnarpvpafbywvdzfukp.supabase.co/storage/v1/object/public/EventImages/1738701932649",
+        "timestamp": DateTime.now().subtract(Duration(minutes: 5)),
+        "type": NotificationType.follow,
+      },
+      {
+        "user": "Usuario2",
+        "userAvatar": "https://crnarpvpafbywvdzfukp.supabase.co/storage/v1/object/public/EventImages/1738701932649",
+        "timestamp": DateTime.now().subtract(Duration(hours: 1)),
+        "type": NotificationType.likeEvent,
+        "eventImage": "https://crnarpvpafbywvdzfukp.supabase.co/storage/v1/object/public/EventImages/1738635570177"
+      },
+      {
+        "user": "Usuario3",
+        "userAvatar": "https://crnarpvpafbywvdzfukp.supabase.co/storage/v1/object/public/EventImages/1738701932649",
+        "timestamp": DateTime.now().subtract(Duration(days: 1)),
+        "type": NotificationType.commentEvent,
+        "eventImage": "https://crnarpvpafbywvdzfukp.supabase.co/storage/v1/object/public/EventImages/1738635570177",
+      },
+    ];
+    setState(() {
+      isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notificaciones'),
-      ),
-      body: ListView.builder(
-        itemCount: 10, // Número de ejemplo de notificaciones
-        itemBuilder: (context, index) {
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: ListTile(
-              leading: const CircleAvatar(
-                child: Icon(Icons.notifications),
-              ),
-              title: Text('Notificación ${index + 1}'),
-              subtitle: Text(
-                  'Esta es una descripción de ejemplo para la notificación ${index + 1}'),
-              trailing: Text('${DateTime.now().hour}:${DateTime.now().minute}'),
-              onTap: () {
-                // Acción al tocar la notificación
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                      content: Text('Notificación ${index + 1} seleccionada')),
-                );
-              },
-            ),
-          );
-        },
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppHeader(),
+        body: Column(
+          children: [
+            isLoading
+                ? Loading()
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: notifications.length ,
+                      itemBuilder: (context, index) {
+                        final item = notifications[index];
+                        return NotificationItem(
+                          user: item['user'],
+                          timestamp: item['timestamp'],
+                          userAvatar: item['userAvatar'],
+                          type: item['type'],
+                          eventImage: item['eventImage'],
+                          onFollow: () => debugPrint("SEGUIR"),
+                        );
+                      },
+                    ),
+                  ),
+          ],
+        ),
       ),
     );
   }
