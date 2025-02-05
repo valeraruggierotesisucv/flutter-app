0709ef38b52ed58594738c5d04e5b7130dd590e9
ruggieroDaniela <chachy.drs@gmail.com>
2025-02-05 16:51:08 -0400

    photo done

--- lib/views/add_view.dart
+++ lib/views/add_view.dart
@@ -1,7 +1,8 @@
 import 'package:eventify/widgets/app_header.dart';
 import 'package:eventify/widgets/image_modal.dart';
-import 'package:eventify/widgets/modal.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class AddView extends StatefulWidget {
   const AddView({super.key});
@@ -11,6 +12,33 @@ class AddView extends StatefulWidget {
 }
 
 class _AddViewState extends State<AddView> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+
+  Future<void> _takePhoto() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    if (photo != null && mounted) {
+      // Aquí puedes manejar la imagen capturada
+      setState(() {
+        _image = photo;
+      });
+      debugPrint('Foto tomada: ${photo.path}');
+      Navigator.of(context).pop();
+    }
+  }
+
+  Future<void> _chooseFromGallery() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.gallery);
+    if (photo != null && mounted) {
+      // Aquí puedes manejar la imagen seleccionada
+      setState(() {
+        _image = photo; 
+      });
+      debugPrint('Foto seleccionada: ${photo.path}');
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,12 +49,24 @@ class _AddViewState extends State<AddView> {
           children: [
             GestureDetector(
               onTap: () {
-                showPhotoModal(context,
-                  onTakePhoto: () => debugPrint("TAKE PHOTO"),
-                  onChooseFromGallery: () => debugPrint("CHOOSE GALERY"),
-                  onClose: () => debugPrint("CERRAR"));
+                showPhotoModal(
+                  context,
+                  onTakePhoto: _takePhoto,
+                  onChooseFromGallery: _chooseFromGallery,
+                  onClose: () {
+                    Navigator.of(context).pop(); // Cierra el modal
+                  },
+                );
               },
-              child: Container(
+              child: 
+              _image != null 
+              ? Container(
+                  width: double.infinity,
+                  height: 250,
+                  decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                  child: Image.file(File(_image!.path)),
+                )
+              : Container(
                 width: double.infinity,
                 height: 250,
                 alignment: Alignment.center,
