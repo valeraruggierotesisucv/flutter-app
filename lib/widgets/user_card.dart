6f8c3497952b1860e121e5a316f2625d33a818af
ruggieroDaniela <chachy.drs@gmail.com>
2025-02-05 09:53:00 -0400

    followersView

--- lib/widgets/user_card.dart
+++ lib/widgets/user_card.dart
@@ -1,3 +1,4 @@
+import 'package:eventify/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 enum UserCardVariant {
@@ -29,40 +30,54 @@ class UserCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 3),
-      child: Row(
-        children: [
-          Expanded(
-            child: GestureDetector(
-              onTap: onPressUser,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 20,
-                      backgroundImage: profileImage != null
-                          ? NetworkImage(profileImage!)
-                          : null,
-                      child: profileImage == null
-                          ? const Icon(Icons.person)
-                          : null,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Text(
-                        username,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: GestureDetector(
+                onTap: onPressUser,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 20,
+                        backgroundImage: profileImage != null
+                            ? NetworkImage(profileImage!)
+                            : null,
+                        child: profileImage == null
+                            ? const Icon(Icons.person)
+                            : null,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          username,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            if (variant == UserCardVariant.withButton) 
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: CustomButton(
+                  label: actionLabel ?? "Action",
+                  onPress: onPressButton,
+                  size: ButtonSize.extraSmall,
+                  fontSize: 14,
+                  disabled: disabled,
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
