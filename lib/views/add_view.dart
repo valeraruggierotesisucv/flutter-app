65f1d7b05da0442044b0406cbc24fac429b877e3
ruggieroDaniela <chachy.drs@gmail.com>
2025-02-07 18:23:23 -0400

    cleanUp

--- lib/views/add_view.dart
+++ lib/views/add_view.dart
@@ -11,8 +11,6 @@ import 'package:eventify/widgets/image_modal.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter_sound/flutter_sound.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 import 'dart:io';
 
