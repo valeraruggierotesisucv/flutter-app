d394a6692baef949c5d6fec2112d2a9b0d72940c
jhoabran <69612177+jhoabran@users.noreply.github.com>
2025-02-07 14:58:40 -0400

    Fix userId property access in profile navigation

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -98,7 +98,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                   context,
                                   MaterialPageRoute(
                                     builder: (context) => ProfileDetailsView(
-                                      userId: event['userId'], 
+                                      userId: event.userId, 
                                     ),
                                   ),
                                 );
